44966c234c3344a109d90795085b6483f80999dc
Ahmed Alwahib <aalwheep@gmail.com>
2023-01-16 00:26:59 +0300

    #01-02 Jobs Endpoint

--- src/main/java/com/cecilireid/springchallenges/SpringChallengesApplication.java
+++ src/main/java/com/cecilireid/springchallenges/SpringChallengesApplication.java
@@ -2,10 +2,8 @@ package com.cecilireid.springchallenges;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class SpringChallengesApplication {
 
 	public static void main(String[] args) {
